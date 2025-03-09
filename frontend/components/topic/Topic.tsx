16e0741278c78ba11d976559849b068729a12140
Moșneguțu Adrian-Ioan <adrianmosnegutuioan@yahoo.com>
2025-03-09 10:12:17 +0200

    refactor: fix topic component
    Made the Topic component more versatile by allowing it to containg a
    react node instead of plain text.

--- frontend/components/topic/Topic.tsx
+++ frontend/components/topic/Topic.tsx
@@ -1,11 +1,13 @@
+import { ReactNode } from "react";
+
 interface Props {
-  topicName: string;
+  children: ReactNode;
 }
 
-export default function Topic({ topicName }: Props) {
+export default function Topic({ children }: Props) {
   return (
     <li className="w-fit rounded-sm bg-white/10 p-3 pt-1 pb-1">
-      <span className="text-sm">{topicName}</span>
+      <span className="text-sm">{children}</span>
     </li>
   );
 }
--- frontend/components/topic/TopicsList.tsx
+++ frontend/components/topic/TopicsList.tsx
@@ -8,7 +8,7 @@ export default function TopicsList({ topics }: Props) {
   return (
     <ul className={`${topics.length === 0 ? "hidden" : ""} flex gap-1`}>
       {topics.map((topic, index) => (
-        <Topic key={index} topicName={topic} />
+        <Topic key={index}>{topic}</Topic>
       ))}
     </ul>
   );
