1c8e567e6af7ad36a61215f0473a711f127dd060
Moșneguțu Adrian-Ioan <adrianmosnegutuioan@yahoo.com>
2025-03-09 11:50:28 +0200

    feat: add sidebar
    Added a sidebar that contains a panel for searching posts based on
    topic, ordering them and a button to create a new post.

---
+++ frontend/components/sidebar/Sidebar.tsx
@@ -0,0 +1,15 @@
+import Button from "../Button";
+import OrderPanel from "./OrderPanel";
+import SearchPanel from "./search-panel/SearchPanel";
+
+export default function Sidebar() {
+  return (
+    <div className="flex w-1/4 flex-col gap-4">
+      <SearchPanel />
+      <OrderPanel />
+      <Button variant="primary" className="w-full">
+        Create Post
+      </Button>
+    </div>
+  );
+}
---
+++ frontend/components/sidebar/search-panel/SearchPanel.tsx
@@ -0,0 +1,7 @@
+export default function SearchPanel() {
+  return (
+    <form
+    >
+    </form>
+  );
+}
