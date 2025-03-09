d3d4b789f8c8a84dba528315b95660661d318188
Moșneguțu Adrian-Ioan <adrianmosnegutuioan@yahoo.com>
2025-03-09 11:51:10 +0200

    feat: add search panel
    Developed the search panel to have core functionality.

--- frontend/components/sidebar/search-panel/SearchPanel.tsx
+++ frontend/components/sidebar/search-panel/SearchPanel.tsx
@@ -1,7 +1,57 @@
+"use client";
+
+import Button from "@/components/Button";
+import { useState } from "react";
+import SelectedTopicsList from "./SelectedTopicsList";
+
 export default function SearchPanel() {
+  const [topic, setTopic] = useState<string>("");
+  const [selectedTopics, setSelectedTopics] = useState<string[]>([]);
+
+  function handleAddTopic(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (
+      !topic ||
+      selectedTopics.some((selectedTopic) => selectedTopic === topic)
+    ) {
+      return;
+    }
+
+    setSelectedTopics((prev) => [topic, ...prev]);
+    setTopic("");
+  }
+
+  function handleDeselectTopic(removedTopic: string) {
+    setSelectedTopics((prev) => prev.filter((topic) => topic !== removedTopic));
+  }
+
+  function handleSearch() {}
+
   return (
-    <form
-    >
-    </form>
+    <div className="bg-custom-gray flex flex-col gap-4 rounded-xl p-6">
+      <form onSubmit={handleAddTopic}>
+        <input
+          title="Topic"
+          type="text"
+          placeholder="Search posts by topic..."
+          maxLength={20}
+          pattern="[0-9a-zA-Z_]+"
+          value={topic}
+          required
+          onChange={(e) => setTopic(e.target.value)}
+          className="w-full rounded-xl border border-gray-400 p-4 pt-2 pb-2"
+        />
+      </form>
+      {selectedTopics.length > 0 && (
+        <SelectedTopicsList
+          selectedTopics={selectedTopics}
+          handleDeselectTopic={handleDeselectTopic}
+        />
+      )}
+      <Button variant="primary" onClick={handleSearch}>
+        Search
+      </Button>
+    </div>
   );
 }
---
+++ frontend/components/sidebar/search-panel/SelectedTopicsList.tsx
@@ -0,0 +1,24 @@
+import RemovableTopic from "@/components/topic/RemovableTopic";
+
+interface Props {
+  selectedTopics: string[];
+  handleDeselectTopic: (topic: string) => void;
+}
+
+export default function SelectedTopicsList({
+  selectedTopics,
+  handleDeselectTopic,
+}: Props) {
+  return (
+    <ul className="flex flex-wrap gap-2">
+      {selectedTopics.map((topic, index) => (
+        <RemovableTopic
+          key={index}
+          onTopicRemoved={() => handleDeselectTopic(topic)}
+        >
+          {topic}
+        </RemovableTopic>
+      ))}
+    </ul>
+  );
+}
